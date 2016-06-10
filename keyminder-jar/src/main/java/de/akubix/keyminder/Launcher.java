2513176500dbed0e77ed220c7217a47dd55380ba
Bastian Kraemer <Bastian.Kraemer@akubix.de>
2016-06-10 16:58:23 +0200

    Store image map in properties file

--- keyminder-jar/src/main/java/de/akubix/keyminder/Launcher.java
+++ keyminder-jar/src/main/java/de/akubix/keyminder/Launcher.java
@@ -2,15 +2,11 @@ package de.akubix.keyminder;
 
 import de.akubix.keyminder.core.ApplicationInstance;
 import de.akubix.keyminder.core.KeyMinder;
-import de.akubix.keyminder.lib.gui.ImageSelector;
 import de.akubix.keyminder.ui.fx.MainWindow;
 
 public class Launcher {
 	public static void main(String[] args) {
 		ApplicationInstance app = KeyMinder.init(args);
-
-		// Provide all icons by keywords
-		ImageSelector.buildIndex();
 
 		if(!KeyMinder.environment.containsKey("console_mode")){
 			// Load the JavaFX UserInterface (It has to call "instance.startup()" when it is ready).
