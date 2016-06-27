01feac2998e85eee3e98a6152306875d31df03c1
Bastian Kraemer <Bastian.Kraemer@akubix.de>
2016-06-27 19:52:49 +0200

    Change JavaFX user interface identifier

--- keyminder-ui/src/main/java/de/akubix/keyminder/ui/fx/JavaFxUserInterface.java
+++ keyminder-ui/src/main/java/de/akubix/keyminder/ui/fx/JavaFxUserInterface.java
@@ -4,7 +4,7 @@ import de.akubix.keyminder.core.ApplicationInstance;
 
 public class JavaFxUserInterface {
 	public static final String LANGUAGE_BUNDLE_KEY = "fxUI";
-	public static final String USER_INTERFACE_ID = "fxUI";
+	public static final String USER_INTERFACE_ID = "JavaFX user interface";
 
 	public static boolean isLoaded(ApplicationInstance app){
 		return app.getUserInterfaceInformation().id().equals(USER_INTERFACE_ID);
